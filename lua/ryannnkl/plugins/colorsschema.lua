e212f9cd674fe675fab22a648be45ca5ddfcb774
Ryannnkl <ryannnkl@gmail.com>
2025-10-04 19:16:00 -0300

    feat: adiciona gerenciador de temas e fixa apenas 1 tema por padrão

--- lua/ryannnkl/plugins/colorsschema.lua
+++ lua/ryannnkl/plugins/colorsschema.lua
@@ -1,61 +1,23 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      local transparent = false -- set to true if you would like to enable transparency
-
-      local bg = "#011628"
-      local bg_dark = "#011423"
-      local bg_highlight = "#143652"
-      local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
-
-      require("tokyonight").setup({
-        style = "night",
-        transparent = transparent,
-        styles = {
-          sidebars = transparent and "transparent" or "dark",
-          floats = transparent and "transparent" or "dark",
-        },
-        on_colors = function(colors)
-          colors.bg = bg
-          colors.bg_dark = transparent and colors.none or bg_dark
-          colors.bg_float = transparent and colors.none or bg_dark
-          colors.bg_highlight = bg_highlight
-          colors.bg_popup = bg_dark
-          colors.bg_search = bg_search
-          colors.bg_sidebar = transparent and colors.none or bg_dark
-          colors.bg_statusline = transparent and colors.none or bg_dark
-          colors.bg_visual = bg_visual
-          colors.border = border
-          colors.fg = fg
-          colors.fg_dark = fg_dark
-          colors.fg_float = fg
-          colors.fg_gutter = fg_gutter
-          colors.fg_sidebar = fg_dark
-        end,
-      })
-
-      -- vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
-  {
-    "f4z3r/gruvbox-material.nvim",
-    name = "gruvbox-material",
-    lazy = true,
-    priority = 1001,
-    opts = {},
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
     priority = 1000,
+  },
+  {
+    "erl-koenig/theme-hub.nvim", -- https://github.com/erl-koenig/theme-hub seletor de temas :ThemeHub
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "rktjmp/lush.nvim",
+    },
+    config = function()
+      require("theme-hub").setup({
+        install_dir = vim.fn.stdpath("data") .. "/theme-hub",
+        auto_install_on_select = true,
+        apply_after_install = true,
+        persistent = false,
+      })
+    end,
   },
 }
