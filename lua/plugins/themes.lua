599e3255582ed11bd186269d88a3ccf03b499b92
Ryannnkl <ryannnkl@gmail.com>
2026-06-07 15:32:10 -0300

    Add gruvbox themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,4 +1,17 @@
 return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    init = function()
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_foreground = "material"
+    end,
+  },
   { "rebelot/kanagawa.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
 }
