6dcdec5b6e2c93d155041e7b775b3a60e17872ac
Ryannnkl <ryannnkl@gmail.com>
2026-06-30 09:23:02 -0300

    Update editor UI indicators

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.signcolumn = "no"
-vim.opt.numberwidth = 2
+vim.opt.signcolumn = "number"
+vim.opt.numberwidth = 1
 
 vim.diagnostic.config({
   signs = false,
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,13 +3,14 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = {
       signs = {
-        add = { text = "" },
-        change = { text = "" },
-        delete = { text = "" },
-        topdelete = { text = "" },
-        changedelete = { text = "" },
-        untracked = { text = "" },
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "^" },
+        changedelete = { text = "~" },
+        untracked = { text = "+" },
       },
+      numhl = true,
       current_line_blame = true,
       current_line_blame_opts = {
         virt_text = true,
